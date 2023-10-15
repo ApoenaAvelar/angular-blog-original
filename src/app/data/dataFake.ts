58f13b2e875680a6ef866a99dca5e91d94cb5571
ApoenaAvelar <apoenajorge@gmail.com>
2023-10-14 21:46:20 -0300

    Vários ajustes no conteúdo e formatação

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,12 +1,50 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Ponto de Partida",
+    "description": "Tudo partiu daqui, da minha mente falante que anseia por transformar tudo em palavras e partilhar o que a toca com profundidade. Mente que não mente e me expõe em potencialidades e vulnerabilidades. Mente que me trouxe até aqui! E aqui estou! Livre, leve, entregue. Centrada, equilibrada, no meu eixo conectada. Vibrante, pulsante, amante de mim mesma em todas as minhas nuances e versões que variam entre os verões. Alinhada com meus propósitos e mantendo em consonância os meus pensamentos, sentimentos e ações. Aqui estou! Consciente, me enxergando como natureza que sou. Harmonizada e alimentada pelos 4 elementos: aterrada em solo fértil e reconhecendo minha base nas raízes mais profundas (porque assim eu posso até me mudar, mas jamais irei me perder), fluida como as águas maleáveis, expandida como o ar em sua volatilidade, e inspirada pela força transformadora do fogo me vejo atenta às minhas potencialidades. Aqui estou! Em constante mudança, aprendendo a aceitar a impermanência das coisas, ensaiando meus passos de dança. Não sei se pronta, mas disposta a abraçar a vida e os presentes lindos que ela me traz em toda sua inconstância.Sou Carina Magalhães, ser em construção disposta a compartilhar meus pontos de vista e levar minhas vistas a diferentes pontos. Mulher, Mãe, Geminiana, Reikiana, Relações Públicas, Gestora de Pessoas e Escritora amadora e amorosa. Muito prazer!"
+    ,
+    "photoCover":"./assets/images/pontoDePartida-cover.jpg"
   },
   {
     "id":"2",
+    "title": "Arte de Rua",
+    "description": "Arte urbana é toda manifestação artística realizada nas ruas como pinturas, grafites, esculturas, estátuas vivas e demais apresentações, não se caracterizando como ações de vandalismo. Essas manifestações acontecem em ambientes públicos (edifícios, casas, túneis, viadutos…) e interagem diretamente com o público passante. O propósito que norteia a prática da arte urbana é trazer acessibilidade, visibilidade e contemplação de uma arte que antes era apenas expressa em lugares ditos “consagrados” como teatros, cinemas, bibliotecas e museus e, portanto, restrito à apenas uma parcela da população. Para os artistas, a arte urbana não precisa de movimento cultural e nem de reconhecimento para acontecer, ela só precisa da rua. Por meio da arte urbana, é possível despertar reflexões no público passante, bem como, comunicar lutas, protestos e denúncias que abrangem questões sociais e políticas.",
+    "photoCover":"./assets/images/arteDeRua-home.jpg"
+  },
+  //TODO: implementar todos os outros links e fotos abaixo
+  {
+    "id":"3",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"5",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"6",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"7",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "bla blabla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"8",
     "title": "Nova Série anunciada no Disney +",
     "description": "bla blabla",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
